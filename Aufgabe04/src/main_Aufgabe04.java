4d9b1a6db1701cd6199fb2cc4e1574390e548e78
Etienne Rebetez <etienne.rebetez@oberwallis.ch>
2010-10-27 23:38:12 +0200

    Solving Aufgabe04.

---
+++ Aufgabe04/src/main_Aufgabe04.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+import java.lang.Math;
+
+
+public class main_Aufgabe04 {
+
+	/**
+	 * Werte für A, B und C verlangen
+	 * 
+	 * Berechen x
+	 * 
+	 * x = ( -B +-(B^2 - 4AC )^(1/2)) / ( 2A )
+	 */
+	public static void main(String[] args) {
+		System.out.println("Aufgabe04\n");		
+
+        double a = readDblInput("--- A ---");
+        double b = readDblInput("--- B ---");
+        double c = readDblInput("--- C ---");               
+        
+        double dblSqrBase = Math.pow( b, 2 ) - 4 * a * c;        
+        
+        if ( dblSqrBase < 0 ) {
+        	System.out.println("Kein reeles Resultat...");
+        } else {
+        	double x1 = ( -b + Math.sqrt( dblSqrBase ) ) / ( 2 * a);
+        	double x2 = ( -b - Math.sqrt( dblSqrBase ) )/ ( 2 * a);
+	        System.out.println("x1:" + x1 + " x2:" + x2);    	
+        }        
+	}
+
+	// Reads from standard Input and return value if it is a number.
+	private static double readDblInput(String strMessage) {
+		double dblInput;
+		String strInput = "";
+		// Creating a BufferReader from standard input.
+		BufferedReader buffRead = new BufferedReader(new InputStreamReader(System.in));
+
+		do {
+			System.out.println(strMessage);
+			// Read input
+			try {
+				strInput = buffRead.readLine();
+			} catch (IOException e) {}			
+
+		} while (!Pattern.matches("-?[0-9]+[.]?[0-9]*", strInput));
+			
+		try { // convert String to double
+			dblInput = Double.valueOf(strInput);
+		} catch (NumberFormatException e) {
+			dblInput = 0;
+		} // error handling
+		
+		return dblInput;
+	}	
+
+}
