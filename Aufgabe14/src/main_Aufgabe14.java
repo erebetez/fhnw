28aedd25bcfb242534074c31cd5f99ed97a71bd2
Etienne Rebetez <etienne.rebetez@oberwallis.ch>
2010-11-19 18:08:54 +0100

    Aufgabe14 init.

---
+++ Aufgabe14/src/main_Aufgabe14.java
@@ -0,0 +1,25 @@
+
+public class main_Aufgabe14 {
+    
+	public static void main(String[] args) {
+		int[] array= {4, 3, 6, 2 , 1, 3, 7, 9};
+		int start = 0;
+		
+		StaightSort(array, start, array.length);
+		
+	}
+	
+	public static void StaightSort(int[] array, int start, int end){
+		
+		
+		int pos;
+		int tempValue;
+		int tempPos;
+		
+		while (start < array.length) {
+			
+			
+		}
+	}
+	
+}
