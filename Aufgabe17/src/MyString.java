aa4714ad2d2581962e9d86faefacc495e26a24d0
Etienne Rebetez <etienne.rebetez@oberwallis.ch>
2010-11-21 14:11:46 +0100

    adding concat and append to myString class

--- Aufgabe17/src/MyString.java
+++ Aufgabe17/src/MyString.java
@@ -10,6 +10,53 @@ public class MyString {
 //		m_string = string;
 //	}
 	
+	public static int lenght(char[] string){
+		int i = 0;
+		try{
+			while( string[i++] != (char) 0 ){				
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+		} // error handling
+		return --i;
+	}
+	
+	public static char[] concat(char[] string, char[] string2){
+		int i = 0;
+
+		char [] newString = new char[lenght(string) + lenght(string2) + 1];
+		
+		for (i = 0; i < lenght(string); ++i ) {
+			newString[i] = string[i];			
+		}
+		
+		for( int j = 0; j < lenght(string2); ++j) {
+			newString[i++] = string2[j];
+		}
+		
+		newString[i] = (char) 0;
+		return newString;		
+	}
+	
+	
+	public static void append(char[] destination, char[] source){
+		int j = 0, i;
+		
+		for( i = lenght(destination); i < totalLenght(destination); ++i) {
+		   try{			  
+		        destination[i] = source[j++];
+		        System.out.println(destination);
+//				if (destination[i] != (char) 0) {
+//					destination[i] = (char) 0; 
+//				}		      
+		   }catch (ArrayIndexOutOfBoundsException e) {
+				if (destination[i] != (char) 0) {
+					destination[i] = (char) 0; 
+				}	
+		   } // error handling
+		}		
+
+	}
+	
 	public static int totalLenght (char[] string){
 		int len = 0;
 		char buff;
@@ -21,8 +68,13 @@ public class MyString {
 		} catch (ArrayIndexOutOfBoundsException e) {
 		} // error handling
 
+		System.out.println("totalcapacity" + len);
 		return len;
 		
+	}
+	
+	public static int compare(char[] string1, char[] string2){
+		return -1;
 	}
 	
 	public void show(){
--- Aufgabe17/src/main_Aufgabe17.java
+++ Aufgabe17/src/main_Aufgabe17.java
@@ -5,12 +5,17 @@ public class main_Aufgabe17 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-      char[] test = generateString("Hallo", 6);
+      char[] test = generateString("Hallo", 18);
+      char[] test2 = generateString(" Welt", 8);
+      char[] test3 = generateString("...", 8);
 
       String original = new String(test);
       System.out.println(original);
       System.out.println(MyString.lenght(test));
-		
+      
+      System.out.println(MyString.concat(test, test2));
+      MyString.append(test, test3);
+	  System.out.println(test);
 	}
 
     public static char[] generateString(String s, int capacity){
