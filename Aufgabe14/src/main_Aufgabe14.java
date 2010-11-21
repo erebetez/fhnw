e43ea25bf034e08d76d9845c5b67e2940f117f7c
Etienne Rebetez <etienne.rebetez@oberwallis.ch>
2010-11-21 14:12:00 +0100

    using simple sort function

--- Aufgabe14/src/main_Aufgabe14.java
+++ Aufgabe14/src/main_Aufgabe14.java
@@ -2,24 +2,43 @@
 public class main_Aufgabe14 {
     
 	public static void main(String[] args) {
-		int[] array= {4, 3, 6, 2 , 1, 3, 7, 9};
+		int[] array= {4, 9, 3, 6, 2 , 1, 3, 7, 2, 1, 2};
 		int start = 0;
 		
+		printArray(array);
 		StaightSort(array, start, array.length);
-		
+		printArray(array);
 	}
 	
 	public static void StaightSort(int[] array, int start, int end){
-		
-		
-		int pos;
+		int pos = start;
 		int tempValue;
 		int tempPos;
-		
-		while (start < array.length) {
-			
-			
+				
+		while (start < (end - 1)) {
+			tempValue = array[start];
+			tempPos = start;
+			for (pos = start; pos < end; ++pos){				
+				if (array[pos] < tempValue ) {
+				  tempValue = array[pos];
+				  tempPos = pos;
+				}
+			}
+			if (array[tempPos] != array[start]) {
+				array[tempPos] = array[start];
+				array[start] = tempValue;
+			}
+			++start;
 		}
+	}
+	
+	private static void printArray(int[] myArray){
+		int i;
+ 		System.out.print("[");
+		for (i = 0; i < myArray.length; ++i) {
+			 System.out.printf("%1$2d", myArray[i]);
+		}
+		System.out.println(" ]");
 	}
 	
 }
