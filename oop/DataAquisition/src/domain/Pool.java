d5851f707e84f44a657dbe5aa64fcb3c5f3de393
Etienne Rebetez <etienne.rebetez@oberwallis.ch>
2010-12-16 21:24:24 +0100

    new Data aquisistion class

---
+++ oop/DataAquisition/src/domain/Pool.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.util.Vector;
+
+public class Pool {
+	private Vector<Data> dataPool;
+
+		public Pool(){
+			dataPool = new Vector<Data>();
+		}
+		
+		public boolean addData(Data data){
+			return this.dataPool.add(data);
+		}
+
+		public int getDataCount() {
+			return this.dataPool.size();
+		}
+		
+		public Data getData(int idx) {
+			if (idx < 0 || idx >= this.dataPool.size()) return null;
+			return this.dataPool.get(idx);
+		}
+	
+	
+}
