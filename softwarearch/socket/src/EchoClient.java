887e5a1e6d6a32583f63ebd36162634e14c81574
Etienne Rebetez <etienne.rebetez@oberwallis.ch>
2011-03-21 17:42:59 +0100

    add socket example to fhnw project

---
+++ softwarearch/socket/src/EchoClient.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class EchoClient {
+	public static void main(String[] args) throws Exception {
+
+		try {
+			Socket echoSocket = null;
+			PrintWriter out = null;
+			BufferedReader in = null;
+	
+			echoSocket = new Socket("localhost", 9001);
+			out = new PrintWriter(echoSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(echoSocket
+					.getInputStream()));
+	
+			BufferedReader stdIn = new BufferedReader(
+						new InputStreamReader(System.in));
+
+
+		
+			while (in!=null) {
+				try{						
+					System.out.print("client> ");
+					out.println(stdIn.readLine());
+					System.out.println("echo from server: '" + in.readLine()+"'");
+						
+		        } catch (Exception e) {
+		        	System.out.println("Fehler " + e.getMessage() + " auf dem Client:");
+		        	System.out.println(e.getStackTrace());
+		        	break;
+		        }					
+			}
+
+		out.close();
+		in.close();
+		echoSocket.close();
+		
+        } catch (Exception e) {
+        	System.out.println("Fehler " +e.getMessage() + " beim initalisieren:");
+        	System.out.println(e.getStackTrace());
+        	
+        }
+	}
+}
---
+++ softwarearch/socket/src/EchoServer.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class EchoServer {
+	public static void main(String[] args) throws Exception {
+
+		ServerSocket serverSocket = null;
+		serverSocket = new ServerSocket(9001);
+        try {
+	        	
+			Socket clientSocket = null;
+			while (serverSocket != null) {
+				clientSocket = serverSocket.accept();
+	
+				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),
+						true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						clientSocket.getInputStream()));
+				String inputLine, outputLine;
+	
+				while ((inputLine = in.readLine()) != null) {
+					outputLine = inputLine;
+					out.println(outputLine);
+				}
+				out.close();
+				in.close();
+				clientSocket.close();
+			}
+	
+			serverSocket.close();
+        } catch (Exception e) {
+        	System.out.println("Fehler" +e.getMessage() + " auf dem Server:");
+        	System.out.println(e.getStackTrace());
+        	serverSocket.close();
+        	
+        }
+	}
+}
